8891916c83cd1af86352bdf1dc013d1b2fd182d9
maoudin <mathis.oudin42@gmail.com>
2025-11-14 14:14:08 +0100

    feat: add api platform

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,6 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
